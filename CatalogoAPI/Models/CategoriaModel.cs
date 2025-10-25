2ac29650067f64b2310e04b0ca91c0ee2fc89a22
Felipe Moura <fe.angelo@outlook.com>
2025-10-25 16:21:39 -0300

    Refatoração de modelos e migrações no banco de dados
    - Renomeada a propriedade `Id` para `CategoriaId` em `CategoriaModel`.
    - Adicionadas validações e restrições de tamanho para `Nome`, `ImagemUrl` e `Descricao` em `CategoriaModel` e `ProdutoModel`.
    - Alterado o tipo de dado de `Preco` para `decimal(10,2)` em `ProdutoModel`.
    - Criadas migrações para ajustes de colunas e inserção de dados iniciais:
      - `20251025185847_AjusteTipoColunas`: ajustes de tipos e validações.
      - `20251025190930_PopulaCategorias`: inserção de categorias iniciais.
      - `20251025191357_PopulaProdutos`: inserção de produtos iniciais.
    - Atualizados arquivos `Designer` para refletir as mudanças.
    - Melhorias gerais no mapeamento e consistência do banco de dados.

--- CatalogoAPI/Models/CategoriaModel.cs
+++ CatalogoAPI/Models/CategoriaModel.cs
@@ -1,9 +1,18 @@
-﻿namespace CatalogoAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace CatalogoAPI.Models;
+
+[Table("Categorias")]
 public class CategoriaModel
 {
-    public int Id { get; set; }
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [Required, StringLength(80)]
     public string Nome { get; set; }
+
+    [Required, StringLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<ProdutoModel>? Produtos { get; set; }
 
--- CatalogoAPI/Models/ProdutoModel.cs
+++ CatalogoAPI/Models/ProdutoModel.cs
@@ -1,14 +1,29 @@
-﻿namespace CatalogoAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace CatalogoAPI.Models;
+
+[Table("Produtos")]
 public class ProdutoModel
 {
+    [Key]
     public int Id { get; set; }
-    public string Nome { get; set; }
+
+    [Required, StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required, StringLength(300)]
     public string? Descricao { get; set; }
+
+    [Required, Column(TypeName ="decimal(10,2)")]
     public decimal Preco { get; set; }
+
+    [Required, StringLength(300)]
     public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
+
+    [ForeignKey("CategoriaId")]
     public int CategoriaId { get; set; }
     public CategoriaModel? Categoria { get; set; }
 }
