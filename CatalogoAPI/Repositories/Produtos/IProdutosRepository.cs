663c7cedfbbb1003ff684e187dd67ece42bafc7d
Felipe Moura <fe.angelo@outlook.com>
2025-11-13 19:31:36 -0300

    Adiciona funcionalidade de paginação na API de produtos
    Inclui uma nova rota no `ProdutosController` para obter produtos paginados, utilizando o método `GetProdutos` do repositório.
    Adiciona metadados de paginação no cabeçalho da resposta HTTP (`X-Pagination`) e mapeia os produtos para `ProdutoDTOResponse`.
    
    Atualiza a interface `IProdutosRepository` com o método `GetProdutos(ProdutosParameter produtosParameter)` e implementa a lógica de paginação no repositório.
    
    Cria a classe genérica `PagedList` para encapsular a lógica de paginação e a classe `ProdutosParameter` para gerenciar os parâmetros de paginação, com validações para `PageNumber` e `PageSize`.
    
    Altera o `launchSettings.json` para abrir o navegador automaticamente ao iniciar o projeto.

--- CatalogoAPI/Repositories/Produtos/IProdutosRepository.cs
+++ CatalogoAPI/Repositories/Produtos/IProdutosRepository.cs
@@ -1,8 +1,10 @@
 ﻿using CatalogoAPI.Models;
+using CatalogoAPI.Pagination;
 using CatalogoAPI.Repositories.Generic;
 
 namespace CatalogoAPI.Repositories.Produtos;
 public interface IProdutosRepository : IRepositoryGeneric<Produto>
 {
+    PagedList<Produto> GetProdutos(ProdutosParameter produtosParameter);
     IEnumerable<Produto> GetProdutosPorCategoria(int categoriaId);
 }
--- CatalogoAPI/Repositories/Produtos/ProdutosRepository.cs
+++ CatalogoAPI/Repositories/Produtos/ProdutosRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CatalogoAPI.Context;
 using CatalogoAPI.Models;
+using CatalogoAPI.Pagination;
 using CatalogoAPI.Repositories.Generic;
 
 namespace CatalogoAPI.Repositories.Produtos;
@@ -8,6 +9,13 @@ public class ProdutosRepository : RepositoryGeneric<Produto>, IProdutosRepositor
 {
     public ProdutosRepository(CatalogoAPIContext context) : base(context)
     {
+    }
+
+    public PagedList<Produto> GetProdutos(ProdutosParameter produtosParameter)
+    {
+        var produtos = GetAll().OrderBy(c => c.ProdutoId).AsQueryable();
+        var produtosOrdenados = PagedList<Produto>.ToPagedList(produtos, produtosParameter.PageNumber, produtosParameter.PageSize);
+        return produtosOrdenados;
     }
 
     public IEnumerable<Produto> GetProdutosPorCategoria(int categoriaId)
